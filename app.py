efa67eafb88d925971dfa7443c193546a473d706
FarJu TUY <jutharat_lifefar@hotmail.com>
2020-11-18 01:31:07 +0700

    edit app.py

--- app.py
+++ app.py
@@ -18,10 +18,10 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-	df= pd.read_csv("bigDATA.csv", encoding="latin-1")
+	df= pd.read_csv("spam.csv", encoding="latin-1")
 	df.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1, inplace=True) #ลบcolumที่ไม่จำเป็นออก
 	# Features and Labels
-	df['label'] = df['v1'].map({'ham': 0, 'phising': 1})
+	df['label'] = df['v1'].map({'ham': 0, 'spam': 1})
 	df['message']=df['v2']
 	df.drop(['v1','v2'],axis=1,inplace=True)
 	X = df['message']
